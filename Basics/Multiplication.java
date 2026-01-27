eb1475d5610f27ebc8bab2ce5c5d594a2c0f0e5c
AtulSingh205 <atulsingh07062004@gmail.com>
2026-01-27 23:46:02 +0530

    updte

---
+++ Basics/Multiplication.java
@@ -0,0 +1,11 @@
+public class Multiplication{
+
+    static void mult(int a){
+        for(int i=1;i<=10;i++){
+              System.out.println(a +" "+ "*"+ i +" "+ i*a);
+        }
+    }
+    public static void main(String[] args) {
+       mult(5);
+    }
+}
