d05ff120559df734c1e9bbd4d8ce9afe40bd4308
AtulSingh205 <atulsingh07062004@gmail.com>
2026-01-28 23:06:03 +0530

    update

---
+++ Basics/Sumofn.java
@@ -0,0 +1,10 @@
+public class Sumofn {
+    public static void main(String[] args) {
+        int a = 10;
+        int sum = 0;
+        for(int i = 1;i<a;i++){
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
