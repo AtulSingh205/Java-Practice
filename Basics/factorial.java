a7e94452759f52ebc6d0733ea093aeb6e6feffc2
AtulSingh205 <atulsingh07062004@gmail.com>
2026-01-29 22:27:55 +0530

    upfrt

---
+++ Basics/factorial.java
@@ -0,0 +1,14 @@
+public class factorial {
+    static int factorial(int n ){
+             if(n==0 || n==1){
+                return 1;
+             }
+             else{
+                return n * factorial(n-1);
+             }
+      }
+
+    public static void main(String[] args) {
+        System.out.println("Factorial numb is "+factorial(5));
+    }
+}
